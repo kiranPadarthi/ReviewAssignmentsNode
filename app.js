26bf3e3c9bab6d2727db291b8e799aad2d36007a
Padarthi <kiran.padarthi@sap.com>
2018-11-22 21:00:11 +0530

    yaml and port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/searchUser/:id', routes.searchUser);
 // app.get('/getusers', routes.getUsersList);
 // app.get('/getreview', routes.getReview);
 //getting the port to run the application
-var port = process.env.PORT || 3998;
+var port = process.env.PORT || 8080;
 //displying port in the console and adding th port to listen
 var server=app.listen(port,function(req,res){
     console.log("http://localhost:"+port);
--- models/db.js
+++ models/db.js
@@ -1,8 +1,7 @@
 var chalk = require('chalk');
 var mongoose = require( 'mongoose' );
-
-var dbURI = 'mongodb://localhost:27017/trainingManagement';
-
+// var dbURI = 'mongodb://localhost:27017/trainingManagement';
+var dbURI = 'mongodb://admin:admin@cluster0-shard-00-00-z530f.mongodb.net:27017,cluster0-shard-00-01-z530f.mongodb.net:27017,cluster0-shard-00-02-z530f.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 mongoose.connect(dbURI,{ useNewUrlParser: true });
 mongoose.connection.on('connected', function () {
     console.log(chalk.green('Mongoose connected to ' + dbURI));
